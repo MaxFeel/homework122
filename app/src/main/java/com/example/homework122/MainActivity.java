b7cc1f5a6f19891cf23fcabda34e912191b33629
maximtosh <mr.maks.filippov.94@mail.ru>
2019-05-21 01:17:01 +0300

    update

--- app/src/main/java/com/example/homework122/MainActivity.java
+++ app/src/main/java/com/example/homework122/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.homework122;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView link = findViewById(R.id.myFileLink);
+        String linkName = "http://myfile.org/";
+        int randomNumber = 1 + (int)(Math.random() * 100) ;
+        String fullLink = linkName + randomNumber;
+        link.setText(fullLink);
+
+        Button btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(nextListner);
+
+        Button btnPrev = findViewById(R.id.btnPrev);
+
     }
+
+    private final View.OnClickListener nextListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent nextScreen = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(nextScreen);
+        }
+    };
+
+    private final View.OnClickListener prevListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Intent prevScreen = new Intent(this, MainActivity.class);
+            //startActivity(prevScreen);
+            finish();
+        }
+    };
+
+
 }
